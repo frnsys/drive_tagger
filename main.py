68e1a0c31a23cafca62cc7c37256b0fcce25cde5
Francis Tseng <f+accounts@frnsys.com>
2019-05-04 12:50:12 -0400

    updates spreadsheet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import html
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -37,7 +38,8 @@ class Drive:
             with open('token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
 
-        self.service= build('drive', 'v3', credentials=creds)
+        self.service = build('drive', 'v3', credentials=creds)
+        self.sheets = build('sheets', 'v4', credentials=creds).spreadsheets()
 
     def list_folder(self, folder_id):
         """List documents in a folder"""
@@ -73,32 +75,54 @@ class Drive:
             resp = self.service.comments().list(pageToken=page, **params).execute()
             comments += resp['comments']
 
+        # Don't include resolved comments
+        comments = [c for c in comments if not c['resolved']]
+
         # Extract tags
         tagged = []
         for c in comments:
             tags = [t.strip('#') for t in TAG_RE.findall(c['content'])]
             if not tags: continue
             text = c['quotedFileContent']['value']
+            text = html.unescape(text)
             tagged.append((text, tags))
         return tagged
 
+    def update_spreadsheet(self, sheet_id, tagged):
+        # Reset spreadsheet
+        self.sheets.values().clear(spreadsheetId=sheet_id, range='A:Z').execute()
+
+        headers = ['Document ID', 'Text', 'Tags']
+        values = [[doc_id, text, ', '.join(tags)] for doc_id, (text, tags) in tagged]
+        body = {
+            'values': [headers] + values
+        }
+        range = '1:{}'.format(len(body['values']))
+        self.sheets.values().update(
+            spreadsheetId=sheet_id,
+            body=body,
+            range=range,
+            valueInputOption='RAW').execute()
 
 if __name__ == '__main__':
     import sys
     try:
         FOLDER_ID = sys.argv[1]
+        SHEET_ID = sys.argv[2]
     except IndexError:
-        print('Please specify the folder ID')
+        print('Please specify the folder and spreadsheet ID')
         sys.exit(1)
 
-    tags = {}
+    tags = []
     drive = Drive()
 
+    print('Reading comments...')
     files = drive.list_folder(FOLDER_ID)
     for f in files:
         doc_id = f['id']
         tagged = drive.get_tags(doc_id)
-        if tagged:
-            tags[doc_id] = tagged
+        tags += [(doc_id, t) for t in tagged]
 
-    import ipdb; ipdb.set_trace()
+    print('Updating spreadsheet...')
+    drive.update_spreadsheet(SHEET_ID, tags)
+    print('Done')
--- readme.md
+++ readme.md
@@ -6,8 +6,10 @@ Will crawl documents in a Google Drive folder and extract tags (anything startin
 
 - Install dependencies: `pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib`
 - Go to <https://developers.google.com/drive/api/v3/quickstart/python> and click "Enable the Drive API" to download a `credentials.json` file. Place that in this folder.
-- Get the ID of the folder you want to crawl. Open the folder in Drive and look at the URL - the ID is the last part, i.e. `https://drive.google.com/drive/u/0/folders/FOLDER_ID`
+- Go to <https://console.developers.google.com/apis/api/sheets.googleapis.com> and enable the Sheets API.
+- Get the ID of the folder you want to crawl. Open the folder in Drive and look at the URL - the ID is the last part, i.e. `https://drive.google.com/drive/u/0/folders/<FOLDER_ID>`
+- Create a spreadsheet that you want to populate with the tagged data. Get that ID, it's also in the URL: `https://docs.google.com/spreadsheets/d/<SHEET_ID>/edit#gid=0`
 
 ## Usage
 
-    python main.py FOLDER_ID
+    python main.py FOLDER_ID SHEET_ID
